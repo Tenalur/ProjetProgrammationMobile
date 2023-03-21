e5270f08289ce7f703113db0735c4c86ed952633
Tenalur <laurent.biton37@outlook.fr>
2023-03-21 19:09:27 +0100

    2premières pages

---
+++ app/src/main/java/com/example/projetprogrammationmobile/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.projetprogrammationmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clic(View v){
+        if(v instanceof Button){
+            ;
+        }
+    }
+}
